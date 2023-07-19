4ad8d596cceac00f1025acedb6a39b7d7305a083
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-07-19 10:43:43 +0300

    Controller

---
+++ Casgem.ApiLayer/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Casgem.BusinessLayer.Abstract;
+using Casgem.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem.ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly  ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        [HttpGet]
+        public IActionResult CustomerList()
+        {
+            var values = _customerService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CustomerAdd(Customer customer)
+        {
+            _customerService.TInsert(customer);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult CustomerDelete(int id)
+        {
+            var value = _customerService.TGetByID(id);
+            _customerService.TDelete(value);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(int id)
+        {
+            var value = _customerService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            _customerService.TUpdate(customer);
+            return Ok();
+        }
+    }
+}
--- Casgem.ApiLayer/Program.cs
+++ Casgem.ApiLayer/Program.cs
@@ -9,6 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+
+builder.Services.AddScoped<ICustomerDal, EfCustomerDal>();
+builder.Services.AddScoped<ICustomerService, CustomerManager>();
+
 builder.Services.AddDbContext<Context>();
 
 
